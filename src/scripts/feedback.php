dd9b0ae4b9480ac065de865da63e1072b3434891
Sher <max.pechko@gmail.com>
2017-09-24 05:32:11 +0300

    0.4.4

--- src/scripts/feedback.php
+++ src/scripts/feedback.php
@@ -5,3 +5,19 @@
  * Date: 26.08.2017
  * Time: 06:27
  */
+
+mb_internal_encoding('utf-8');
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+$to = "joncolab@gmail.com";
+$headers = "From: " . $name . " <" . $email . ">";
+
+if (mail($to, $subject, $message, $headers)) {
+    echo 'success';
+} else {
+    echo 'error';
+}
+
+exit();
