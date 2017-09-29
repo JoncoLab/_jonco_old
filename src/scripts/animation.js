b5279303715e08e7cf263df69f1907fe08383799
Sher <max.pechko@gmail.com>
2017-09-29 23:23:24 +0300

    0.5.2

---
+++ src/scripts/animation.js
@@ -0,0 +1,13 @@
+const animation = function () {
+    // const animationTarget = $('#about-visual img'),
+    //     randNum = Math.floor(Math.random() * 100);
+    //
+    // let animationTimer = setTimeout(function move() {
+    //     animationTarget.style.right = randNum + 'px';
+    //     animationTimer = setTimeout(move, 4000);
+    // }, 4000);
+    //
+    // animationTimer();
+};
+
+$(document).ready(animation);
